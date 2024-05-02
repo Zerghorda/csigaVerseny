7b08d44aa3f650a5b134c17f3bfd4e568358ecb1
Tar Gergő <tar.gergo21@gmail.com>
2024-05-02 09:49:06 +0200

    csiga fix

--- CsigaVerseny/src/csigaverseny/Csiga.java
+++ CsigaVerseny/src/csigaverseny/Csiga.java
@@ -8,47 +8,19 @@ public class Csiga {
     String szin;
     Random rnd = new Random();
 
-    void mozgas() {
-        lepes = rnd.nextInt(0, 4);
-        osszlepes += lepes;
-        String[] szinek = {"Zöld", "Piros", "Kék"};
-        szin = szinek[rnd.nextInt(0, szinek.length)];
-        return lepes;
-    }
-    
-    
-    public int getOsszlepes() {
-        return osszlepes;
-    }
-
-    public String getSzin() {
-        return szin;
-    }
-
-
-    }
-
-    public static int getLepes() {
-        return lepes;
-    }
-
-    public static int getOsszlepes() {
-        return osszlepes;
-    }
-
-    public static String getSzin() {
-        return szin;
-    }
-
-    private static int lepes;
-    private static int osszlepes;
-    private static String szin;
-    static Random rnd = new Random();
-
     void mozgas(int szam) {
         lepes = szam * rnd.nextInt(0, 4);
         osszlepes += lepes;
+        String[] szinek = {"Zöld", "Piros", "Kék"};
+        szin = szinek[rnd.nextInt(0, szinek.length)];
+    }
 
+    private int getOsszlepes() {
+        return osszlepes;
+    }
+
+    private String getSzin() {
+        return szin;
     }
 
     void mozgas() {
